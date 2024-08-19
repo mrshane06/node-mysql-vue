01999255122bd953e4c122e30e7ba60cc214e6f0
mrshane06 <niekerkkeegan@007gmail.com>
2024-08-19 09:49:18 +0200

    config

--- config/config.js
+++ config/config.js
@@ -6,7 +6,9 @@ const pool = createPool({
     host: process.env.HOST,
     user: process.env.USER,
     database:process.env.DATABASE,
-    password:process.env.PASSWORD
+    password:process.env.PASSWORD,
+    secret_key:process.env.SECRET_KEY,
+    port:process.env.PORT
 })
 
 export {pool}
